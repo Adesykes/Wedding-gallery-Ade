3b75b1525dc367e018d0d5da6766eeeec4967c48
Adesykes <sykesa@sky.com>
2025-06-10 21:40:01 +0000

    React change

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';  // This matches the export default in App.js
 
+const API_BASE = process.env.REACT_APP_API_BASE;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
